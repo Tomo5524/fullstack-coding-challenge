235dcd30af56f7f551f5fa3c35c0b7db32528a81
Tomo5524 <ttoommoosrst@gmail.com>
2022-12-15 23:34:58 +0900

    created card navigation buttons

---
+++ client/src/components/commons/nav-button/style.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+// import { media } from "../../theme/breakpoints";
+
+export const Button = styled.button`
+  background-color: transparent;
+  background-image: none;
+  border: none;
+  padding: 8px 24px;
+  width: 80px;
+  margin: 32px 16px;
+  &:hover {
+    background-color: #eeecec;
+    box-shadow: 0 0 15px rgb(45 45 45 / 10%);
+    border-radius: 10px;
+  }
+  cursor: pointer;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+`;
