823c106b62c23c082ff06217eb7c34bb26f1e7a8
Tomo5524 <ttoommoosrst@gmail.com>
2022-12-16 01:59:22 +0900

    did some cleaing up

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,11 +1,11 @@
-import UserModel, { UserDocument, UserInput } from "../models/user.model";
+import { UserInput } from "../models/user.model";
 import { collections } from "./database.service";
 
 export async function CreateUser(input: UserInput) {
   try {
     // const user = await UserModel.create(input);
     const user = await collections!.users!.insertOne(input);
-    console.log("🚀 ~ file: user.service.ts:8 ~ CreateUser ~ user", user);
+
     // to remove all records Of a collection
     // await collections!.users!.deleteMany({});
     return user;
