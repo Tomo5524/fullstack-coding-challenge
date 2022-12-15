6aca3ef02f3de740ee8bbe6ee1aa85328f64c421
Tomo5524 <ttoommoosrst@gmail.com>
2022-12-16 01:55:44 +0900

    created validation schema

---
+++ server/src/schema/user.schema.ts
@@ -0,0 +1,51 @@
+import { object, string, TypeOf } from "zod";
+
+export const createUserSchema = object({
+  body: object({
+    product: string({
+      required_error: "product is required",
+    }),
+    option: string({
+      required_error: "option is required",
+    }),
+    user: object({
+      firstName: string({
+        required_error: "firstName is required",
+      }),
+      lastName: string({
+        required_error: "lastName is required",
+      }),
+      fnameKana: string({
+        required_error: "fnameKana is required",
+      }),
+      lnameKana: string({
+        required_error: "lnameKana is required",
+      }),
+      gender: string({
+        required_error: "gender is required",
+      }),
+      age: string({
+        required_error: "gender is required",
+      })
+        .min(1, "Age too small - should be 2 chars minimum")
+        .max(2, "Age too big - cannot be more than 2 chars"),
+      vehicleType: string({
+        required_error: "vehicleType is required",
+      }),
+      postCode: string({
+        required_error: "gender is required",
+      }).max(7, "Age too big - cannot be more than 7 chars"),
+      city: string({
+        required_error: "city is required",
+      }),
+      address: string({
+        required_error: "city is required",
+      }),
+      emailAddress: string({
+        required_error: "Email is required",
+      }).email("Not a valid email"),
+    }),
+  }),
+});
+
+export type CreateUserInput = TypeOf<typeof createUserSchema>;
