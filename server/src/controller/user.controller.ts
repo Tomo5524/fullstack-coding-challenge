37c6fb5eaa86de3310c490cd738dab7df3037c2a
Tomo5524 <ttoommoosrst@gmail.com>
2022-12-16 01:57:10 +0900

    addded validation middleware

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -1,19 +1,17 @@
 import { Request, Response } from "express";
 import { UserInput } from "../models/user.model";
+import { CreateUserInput } from "../schema/user.schema";
 import { CreateUser } from "../services/user.service";
-// import { CreateUserInput } from "../schema/user.schema";
-// import { createUser } from "../service/user.service";
 
-// export async function createUserHandler(
-//   req: Request<{}, {}, CreateUserInput["body"]>,
-//   res: Response
-// )
-export async function createUserHandler(req: Request, res: Response) {
-  const { selectedProduct, userInfo, selectedOption } = req.body.outputData;
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput["body"]>,
+  res: Response
+) {
+  const { option, user, product } = req.body;
   const userInfoData: UserInput = {
-    product: selectedProduct,
-    user: userInfo,
-    option: selectedOption,
+    product,
+    user,
+    option,
   };
   try {
     const result = await CreateUser(userInfoData);
@@ -23,7 +21,6 @@ export async function createUserHandler(req: Request, res: Response) {
           msg: `Successfully created a new game with id ${result.insertedId}`,
         })
       : res.status(500).send("Failed to create a new game.");
-    // do i need to return?
   } catch (error: any) {
     console.error(error);
     return res.status(400).send(error.message);
