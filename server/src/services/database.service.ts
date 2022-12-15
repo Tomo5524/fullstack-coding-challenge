36427e10e29feb083d778a79313a3428cb27157b
Tomo5524 <ttoommoosrst@gmail.com>
2022-12-16 00:04:46 +0900

    created database connect service

---
+++ server/src/services/database.service.ts
@@ -0,0 +1,25 @@
+import * as mongoDB from "mongodb";
+import dotenv from "dotenv";
+
+export const collections: { users?: mongoDB.Collection } = {};
+
+export async function connectToDatabase() {
+  dotenv.config();
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(
+    process.env.MONGODB_URL as string,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: mongoDB.ServerApiVersion.v1,
+    } as mongoDB.ConnectOptions
+  );
+  await client.connect();
+  const db: mongoDB.Db = client.db(process.env.DB_NAME);
+  const usersCollection: mongoDB.Collection = db.collection(
+    process.env.COLLECTION_NAME as string
+  );
+  collections.users = usersCollection;
+  // console.log(
+  //   `Successfully connected to database: ${db.databaseName} and collection: ${usersCollection.collectionName}`
+  // );
+}
